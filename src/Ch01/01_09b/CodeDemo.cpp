1f35245589767d138ca9dc0f03f35dce52355fce
Elina K <145558996+mitanuriel@users.noreply.github.com>
2024-12-18 22:49:31 +0000

    used input and extraction operator

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -5,7 +5,11 @@
 #include <iostream>
 
 int main(){
+    std::string name;
     std::cout << "Hi There!" << std::endl;
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hi there " << name;
 
     std::cout << std::endl << std::endl;
     return 0;
