f1d0386a4096da19edbeaae49663d08299148e31
Elina K <145558996+mitanuriel@users.noreply.github.com>
2024-12-18 21:00:12 +0000

    added hello world

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,11 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include<iostream>
+
+
+int main(){
+    std::cout << "Hello there and good luck" << std::endl;
+    return 0;
+}
